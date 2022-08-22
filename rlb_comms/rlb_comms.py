2d97850d167d3af36941b8b7f6f1e18acd302af8
V.G <victor.guillet@protonmail.com>
2022-08-22 16:24:54 +0200

    initial commit

---
+++ rlb_comms/rlb_comms.py
@@ -0,0 +1,253 @@
+
+
+##################################################################################################################
+"""
+"""
+
+# Built-in/Generic Imports
+import json
+import os
+import queue
+import math
+from functools import partial
+import json
+
+import sys
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist, PoseStamped, Point
+from sensor_msgs.msg import JointState, LaserScan
+from rlb_utils.msg import Goal, TeamComm, CommsState
+from rclpy.qos import QoSProfile, QoSReliabilityPolicy, QoSHistoryPolicy
+import numpy as np
+import math
+
+from rlb_coordinator.Caylus_map_loader import load_maps
+from rlb_coordinator.Raster_ray_tracing import check_comms_available
+from rlb_controller.simulation_parameters import *
+
+##################################################################################################################
+
+
+class RLB_comms(Node):
+    def __init__(self):
+        super().__init__('Comms_sim')
+
+        # -> Setup storage dicts
+        self.team_members = {}
+        self.comm_rays = {}
+
+        # -> Load obstacle_grids
+        obstacle_grids = load_maps(
+            hard_obstacles=True,
+            dense_vegetation=True,
+            light_vegetation=True,
+            paths=False
+        )
+
+        # -> Generate signal blocking probability grid
+        self.signal_blocking_prob_grid = \
+                obstacle_grids["hard_obstacles"] * hard_obstacles_signal_blocking_prob \
+                + obstacle_grids["dense_vegetation"] * dense_vegetation_signal_blocking_prob \
+                + obstacle_grids["light_vegetation"] * light_vegetation_signal_blocking_prob
+
+        # -> Clip min/max values
+        self.signal_blocking_prob_grid = np.clip(self.signal_blocking_prob_grid, 0, 1)
+
+        # ----------------------------------- Team communications subscriber
+        qos = QoSProfile(
+            reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_RELIABLE,
+            history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_ALL,
+            )
+
+        self.team_comms_subscriber = self.create_subscription(
+            msg_type=TeamComm,
+            topic="/team_comms",
+            callback=self.team_msg_subscriber_callback,
+            qos_profile=qos
+            )
+        
+        # ----------------------------------- Comms state publishers timer
+        self.comms_state_pub_timer = self.create_timer(
+            timer_period_sec=0.000001,
+            callback=self.publish_comms_states
+        )
+
+    def publish_comms_states(self):
+        # -> Update comms states
+        self.check_comms_state()
+
+        # -> Publish comms states
+        for agent in self.team_members.keys():
+            msg = CommsState()
+            msg.robot_id = agent
+
+            comms_state = {}   
+
+            for agent_pair in self.agent_pairs:
+                if agent in agent_pair:
+                    other_agent = agent_pair[0] if agent_pair[0] != agent else agent_pair[1]
+                    comms_state[other_agent] = {
+                        "comm_state": self.comm_rays[agent_pair]["comm_state"],
+                        "comms_integrity_profile": self.comm_rays[agent_pair]["comms_integrity_profile"]
+                    }
+            msg.comms_states = json.dumps(comms_state)
+
+            # -> Publish msg
+            self.team_members[agent]["comm_state_publisher"].publish(msg)
+
+    def convert_coords_room_to_pixel(self, point_room):
+        from rlb_controller.simulation_parameters import images_shape
+        from rlb_controller.room_paramters import room_x_range, room_y_range
+
+        # -> Calculating differences
+        dx_img = images_shape[0]
+        dy_img = images_shape[1]
+
+        dx_canvas = abs(room_x_range[0]) + abs(room_x_range[1])
+        dy_canvas = abs(room_y_range[0]) + abs(room_y_range[1])
+
+        aspect_ratio = self.signal_blocking_prob_grid.shape[1]/self.signal_blocking_prob_grid.shape[0]
+        
+        if aspect_ratio < 1:
+            dx_canvas *= aspect_ratio
+        else:
+            dy_canvas *= aspect_ratio
+
+        # -> Solving for scaling factor
+        dx_img_shift = dx_img/dx_canvas
+        dy_img_shift = dy_img/dy_canvas
+
+        return (int(point_room[0] * dx_img_shift + dx_img/2), int(point_room[1] * dy_img_shift + dy_img/2))
+    
+    @property
+    def agent_pairs(self):
+        agent_list = list(self.team_members.keys())
+        agent_pairs = [(a, b) for idx, a in enumerate(agent_list) for b in agent_list[idx + 1:]]
+
+        return agent_pairs
+
+    def team_msg_subscriber_callback(self, msg):
+        if msg.robot_id not in self.team_members.keys():
+            self.add_robot(msg=msg)
+
+    def check_comms_state(self):
+        # -> Check if comms are available for every agent tracked
+        for agent_pair in self.agent_pairs:
+            x1 = round(self.team_members[agent_pair[0]]["pose"]["x"], 3)
+            y1 = round(self.team_members[agent_pair[0]]["pose"]["y"], 3)
+
+            x2 = round(self.team_members[agent_pair[1]]["pose"]["x"], 3)
+            y2 = round(self.team_members[agent_pair[1]]["pose"]["y"], 3)
+
+            # -> Check comms state
+            point_1_pix = self.convert_coords_room_to_pixel(point_room=(x1, y1))
+            point_2_pix = self.convert_coords_room_to_pixel(point_room=(x2, y2))
+
+            self.comm_rays[agent_pair]["comm_state"], self.comm_rays[agent_pair]["comms_integrity_profile"], ray_coordinates = check_comms_available(
+                pose_1=point_1_pix,
+                pose_2=point_2_pix,
+                obstacle_probabilities_grid=self.signal_blocking_prob_grid)
+
+    def pose_subscriber_callback(self, robot_id, msg):
+        # -> Update position
+        self.team_members[robot_id]["pose"]["x"] = msg.pose.position.x
+        self.team_members[robot_id]["pose"]["y"] = msg.pose.position.y
+        self.team_members[robot_id]["pose"]["z"] = msg.pose.position.z
+
+        # -> Update orientation
+        u, v, w = self.__euler_from_quaternion(quat=msg.pose.orientation)
+
+        self.team_members[robot_id]["pose"]["u"] = u
+        self.team_members[robot_id]["pose"]["v"] = v
+        self.team_members[robot_id]["pose"]["w"] = w
+
+    def add_robot(self, msg):
+        # ---------------- Add team member entry to team members dict
+        self.team_members[msg.robot_id] = {
+            # -> Pose setup
+            "pose_subscriber": None,
+            "pose": {
+                "x": 0.,
+                "y": 0.,
+                "z": 0.,
+                "u": 0.,
+                "v": 0.,
+                "w": 0.
+                },
+            
+            # -> Comms setup
+            "comm_state_publisher": None
+        }
+
+        # -> Create comm state publisher
+        qos = QoSProfile(depth=10)
+
+        self.team_members[msg.robot_id]["comm_state_publisher"] = self.create_publisher(
+            msg_type=CommsState,
+            topic=f"/{msg.robot_id}/Comms_state",
+            qos_profile=qos
+        )
+
+        # ---------------- Add team member entry to comm_rays
+        for agent_pair in self.agent_pairs:
+            if agent_pair not in self.comm_rays.keys():
+                self.comm_rays[agent_pair] = {
+                    "comm_state": True,
+                    "comms_integrity_profile": [],
+                    }
+
+        # -> Create pose subscribers
+        qos = QoSProfile(
+            reliability=QoSReliabilityPolicy.RMW_QOS_POLICY_RELIABILITY_BEST_EFFORT,
+            history=QoSHistoryPolicy.RMW_QOS_POLICY_HISTORY_KEEP_LAST,
+            depth=1
+            )
+
+        self.team_members[msg.robot_id]["pose_subscriber"] = self.create_subscription(
+            msg_type=PoseStamped,
+            topic=f"/{msg.robot_id}/pose",
+            callback=partial(self.pose_subscriber_callback, msg.robot_id),
+            qos_profile=qos
+            )
+
+    @staticmethod
+    def __euler_from_quaternion(quat):
+        """
+        Convert quaternion (w in last place) to euler roll, pitch, yaw (rad).
+        quat = [x, y, z, w]
+
+        """
+        x = quat.x
+        y = quat.y
+        z = quat.z
+        w = quat.w
+
+        sinr_cosp = 2 * (w * x + y * z)
+        cosr_cosp = 1 - 2 * (x * x + y * y)
+        roll = np.arctan2(sinr_cosp, cosr_cosp) * 180 / math.pi
+
+        sinp = 2 * (w * y - z * x)
+        pitch = np.arcsin(sinp) * 180 / math.pi
+
+        siny_cosp = 2 * (w * z + x * y)
+        cosy_cosp = 1 - 2 * (y * y + z * z)
+        yaw = np.arctan2(siny_cosp, cosy_cosp) * 180 / math.pi
+
+        return roll, pitch, yaw
+
+
+def main(args=None):
+    # `rclpy` library is initialized
+    rclpy.init(args=args)
+
+    path_sequence = RLB_comms()
+
+    rclpy.spin(path_sequence)
+
+    path_sequence.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
